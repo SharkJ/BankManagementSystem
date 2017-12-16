94864b46e2dde7331e689c0b44d46bb7bec2fcee
random-Amanda <34590815+random-Amanda@users.noreply.github.com>
2017-12-16 10:59:20 +0530

    Update AccountHolder.java

--- AccountHolder.java
+++ AccountHolder.java
@@ -1,4 +1,14 @@
 
 public class AccountHolder {
+  String accNo,name,address,telNo,dob,pin;
+	AccountHolder(String accNo,String name,String address,String telNo,String dob,String pin)
+	{
+		this.accNo= accNo;
+		this.name=name;
+		this.address= address;
+		this.telNo= telNo;
+		this.dob= dob;
+		this.pin= pin;
+	}
 
 }
