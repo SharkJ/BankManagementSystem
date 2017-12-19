b51bccceaa61494b437956fe1cba040dd67ff9c4
SharkJ <shakyani.j@gmail.com>
2017-12-20 00:32:45 +0530

    Update AccountHolder.java
    Added getters & setters

--- AccountHolder.java
+++ AccountHolder.java
@@ -1,6 +1,8 @@
 
 public class AccountHolder {
-  String accNo,name,address,telNo,dob,pin;
+  private String accNo,name,address,telNo,dob,pin;
+	AccountHolder(){
+	}
 	AccountHolder(String accID,String name,String address,String telNo,String dob,String pin)
 	{
 		this.accID= accID;
@@ -9,6 +11,48 @@ public class AccountHolder {
 		this.telNo= telNo;
 		this.dob= dob;
 		this.pin= pin;
+	}
+	public void setAccNo(String accNo){
+		accNo=accNo;
+	}
+	
+	public String getAccNo(){
+		return accNo;
+	}
+	public void setName(String name){
+		name=name;
+	}
+	
+	public String getName(){
+		return accNo;
+	}
+	public void setAddress(String address){
+		address=address;
+	}
+	
+	public String address(){
+		return address;
+	}
+	public void setTelNo(int telNo){
+		telNo=telNo;
+	}
+	
+	public int getTelNo(){
+		return telNo;
+	}
+	public void setDob(int dob){
+		dob=dob;
+	}
+	
+	public int getDob(){
+		return dob;
+	}
+	public void setPin(String pin){
+		accNo=accNo;
+	}
+	
+	public String getPin(){
+		return pin;
 	}
 
 }
