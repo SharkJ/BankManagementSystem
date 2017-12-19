7c4ebddfb2434305e9be7dfa6b376fabbb5020d5
random-Amanda <34590815+random-Amanda@users.noreply.github.com>
2017-12-19 20:18:14 +0530

    Update CreateAccount.java

--- CreateAccount.java
+++ CreateAccount.java
@@ -6,26 +6,23 @@ public class CreateAccount {
 		ConnectToDatabase ob= new ConnectToDatabase();
 		ob.connect();
 		
-		try{
-	      
-	      //STEP 4: Execute a query
-	      
-	      
-	      String sql = "INSERT INTO accountholder " +
-	                   "VALUES ('H0009', 'suvimalee')";
+		try{     
+                  String sql = "INSERT INTO accountholder " +
+                  "VALUES ('H0001', 'Randombage Prabhani Amanda')";	  
+	     
 	      ob.stmt.executeUpdate(sql);
 	     
 	      
 	      System.out.println("Inserted records into the table...");
 	   
 	   }catch(SQLException se){
-	       //Handle errors for JDBC
+	      
 	       se.printStackTrace();
 	    }catch(Exception e){
-	       //Handle errors for Class.forName
+	       
 	       e.printStackTrace();
 	    }
 	   ob.disconnect();
-	   System.out.println("Goodbye!");
+	   System.out.println("Account created");
 	   }
 }
